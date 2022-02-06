f5311e0a20117f2328f4f23a6b3d6d662193f2c0
HenriqueBilo <henrique-bilo@hotmail.com>
2022-02-06 04:00:31 -0300

    Capa do filme com Sombras

--- app/src/main/java/com/bilo/netflixremake/MovieActivity.java
+++ app/src/main/java/com/bilo/netflixremake/MovieActivity.java
@@ -2,8 +2,13 @@ package com.bilo.netflixremake;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MovieActivity extends AppCompatActivity {
 
@@ -19,6 +24,17 @@ public class MovieActivity extends AppCompatActivity {
         if(getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Exibir o icone de Home na toolbar
             getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24);
+        }
+
+        //Para mudar a imagem que aparece juntamente com as Shadows
+        LayerDrawable drawable =  (LayerDrawable) ContextCompat.getDrawable(this, R.drawable.shadows); //Para conseguir modificar um elemento drawable
+
+        if(drawable != null){
+
+            //Acessa o Item da Layer List e troca a propridade drawable
+            Drawable movieCover = ContextCompat.getDrawable(this, R.drawable.movie);
+            drawable.setDrawableByLayerId(R.id.cover_drawble, movieCover);
+            ((ImageView) findViewById(R.id.image_view_cover)).setImageDrawable(drawable); //Efetua a troca de imagem
         }
     }
 }
